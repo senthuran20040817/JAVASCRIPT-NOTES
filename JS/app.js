148eaff55ae84de36ee1a9ec5d3cd39b0f310fcf
Perinpam Senthuran <senthu200408@gmail.com>
2026-09-15 14:34:55 +0530

    DO-WHILE loop

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
-//----------------  WHILE LOOP  ----------------
-let num=2;
+//---------------- DO-WHILE LOOP  ----------------
+let num=4;
 let by=1;
-while(by<=10){
+do{
     console.log(num+" x "+by+" = "+(num*by));
     by++;
-}
+}while(by<=10);
