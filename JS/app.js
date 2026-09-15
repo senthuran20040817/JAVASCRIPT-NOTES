4bd72a6d623b97d9bd2c2495fbe12110e20b0875
Perinpam Senthuran <senthu200408@gmail.com>
2026-09-15 14:45:46 +0530

    Function

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,21 @@
-//---------------- DO-WHILE LOOP  ----------------
-let num=4;
-let by=1;
-do{
-    console.log(num+" x "+by+" = "+(num*by));
-    by++;
-}while(by<=10);
+//---------------- FUNCTIONS  ----------------
+function add(a, b){
+    console.log(a+" + "+b+" = "+(a+b));
+}
+
+function substract(a, b){
+    console.log(a+" - "+b+" = "+(a-b));
+}
+
+function multiply(a, b){
+    console.log(a+" x "+b+" = "+(a*b));
+}
+
+function divide(a, b){
+    console.log(a+" / "+b+" = "+(a/b));
+}
+
+add(10, 2);
+substract(10, 2);
+multiply(10, 2);
+divide(10, 2);
