d00b50400c773671e004c0d29f87a38f17705c33
Perinpam Senthuran <senthu200408@gmail.com>
2026-09-15 15:05:53 +0530

    Printing the items in the array

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,4 @@
 //---------------- ARRAYS  ----------------
 let array = [1, 2, 3, "Senthuran" , "Bineth" , "Sameera" , 1.23 , 2.34 , 3.45 , true , false];
 
-for(let i=0 ; i<array.length ; i++){
-    console.log(array[i]);
-}
+console.log(array);
