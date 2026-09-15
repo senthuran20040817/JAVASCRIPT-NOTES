9ace48d96f3a14046481104a6ba7101dab6938f7
Perinpam Senthuran <senthu200408@gmail.com>
2026-09-15 14:59:40 +0530

    Arrow Function - Another Defining Method

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,9 @@
 //---------------- FUNCTIONS  ----------------
-let add = function(a, b){
-    console.log(a+" + "+b+" = "+(a+b));
-}
+let add = (a, b) => console.log(a+" + "+b+" = "+(a+b));
+let substract = (a, b) => console.log(a+" - "+b+" = "+(a-b));
+let multiply = (a, b) => console.log(a+" x "+b+" = "+(a*b));
+let divide = (a, b) => console.log(a+" / "+b+" = "+(a/b));
 
-let substract = function(a, b){
-    console.log(a+" - "+b+" = "+(a-b));
-}
-
-let multiply = function(a, b){
-    console.log(a+" x "+b+" = "+(a*b));
-}
-
-let divide = function(a, b){
-    console.log(a+" / "+b+" = "+(a/b));
-}
 add(10, 2);
 substract(10, 2);
 multiply(10, 2);
