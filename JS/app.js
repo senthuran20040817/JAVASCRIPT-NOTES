722964adfe4695db556dbfeed23b1fa9d4c55d25
Perinpam Senthuran <senthu200408@gmail.com>
2026-09-15 11:21:30 +0530

    Finding Type of a Variable

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+let a=10;
+let b="Senthuran";
+let c=true;
+let d=2.45;
+let e='x';
+let f=[1, 2, 3, 4, 5];
+let g={}
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
