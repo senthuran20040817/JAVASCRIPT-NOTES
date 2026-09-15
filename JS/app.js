8cca19f5d99971fbb8cdcae1b2918c10bc0f53bc
Perinpam Senthuran <senthu200408@gmail.com>
2026-09-15 12:24:21 +0530

    IF conditional Bill Calculating Excercise

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,12 @@
 //----------------  IF / ELSE / ELSE IF  ----------------
-let marks = 50;
+let units = 110;
+let bill;
 
-if(marks<=100 && marks>=0){
-    if(marks>=75){
-        console.log("Grade : A");
-    }else if(marks>=65){
-        console.log("Grade : B");
-    }else if(marks>=50){
-        console.log("Grade : C");
-    }else if(marks>=35){
-        console.log("Grade : S");
-    }else{
-        console.log("Grade : F");
-    }
+if(units<=10){
+    bill = units*10;
+}else if(units<=100){
+    bill = 50*10 + (units-50)*15;
 }else{
-    console.log("Invlaid Marks");
+    bill = 50*10 + 50*15 + (units-100)*20;
 }
+console.log(bill);
