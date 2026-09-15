973b6f502eceba16acc5876aeea103609b9eea3b
Perinpam Senthuran <senthu200408@gmail.com>
2026-09-15 11:22:12 +0530

    Object Usage

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,9 @@
-let a=10;
-let b="Senthuran";
-let c=true;
-let d=2.45;
-let e='x';
-let f=[1, 2, 3, 4, 5];
-let g={}
+let g={
+    name:"senthuran",
+    age:22,
+    city:"chilaw",
+}
 
-console.log(typeof a);
-console.log(typeof b);
-console.log(typeof c);
-console.log(typeof d);
-console.log(typeof e);
-console.log(typeof f);
-console.log(typeof g);
+console.log(g.name);
+console.log(g.age);
+console.log(g.city);
