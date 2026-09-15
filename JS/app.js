ead28aef7f1f0e7dc2464fbffc59112c4c39b063
Perinpam Senthuran <senthu200408@gmail.com>
2026-09-15 14:49:32 +0530

    Return Type Functions

--- JS/app.js
+++ JS/app.js
@@ -1,21 +1,21 @@
 //---------------- FUNCTIONS  ----------------
 function add(a, b){
-    console.log(a+" + "+b+" = "+(a+b));
+    return a+b;
 }
 
 function substract(a, b){
-    console.log(a+" - "+b+" = "+(a-b));
+    return a-b;
 }
 
 function multiply(a, b){
-    console.log(a+" x "+b+" = "+(a*b));
+    return a*b;
 }
 
 function divide(a, b){
-    console.log(a+" / "+b+" = "+(a/b));
+    return a/b;
 }
 
-add(10, 2);
-substract(10, 2);
-multiply(10, 2);
-divide(10, 2);
+console.log("Addition       => "+add(10, 2));
+console.log("Substraction   => "+substract(10, 2));
+console.log("Multiplication => "+multiply(10, 2));
+console.log("Division       => "+divide(10, 2));
